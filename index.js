367021e001386e461c8b23ec60920c098d986fdb
Eddybrando Vásquez <eddybrando@ibm.com>
2020-04-24 22:20:38 +0200

    feat: add "possible errors" rules

--- index.js
+++ index.js
@@ -19,6 +19,13 @@ module.exports = {
     'no-console': 'off',
     'no-constant-condition': 'error',
     'no-control-regex': 'error',
+    'no-debugger': 'error',
+    'no-dupe-args': 'error',
+    'no-dupe-else-if': 'error',
+    'no-dupe-keys': 'error',
+    'no-duplicate-case': 'error',
+    'no-empty': 'error',
+    'no-empty-character-class': 'error',
     /**
      * Stylistic Issues
      * https://eslint.org/docs/rules/#stylistic-issues
